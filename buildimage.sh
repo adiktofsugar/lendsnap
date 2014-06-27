553588762bfd840e050c9f189ba9e53df6aa5129
Sandeep Puri <lexlapax@gmail.com>
2014-06-26 23:14:54 -0700

    update buildimage.sh

--- buildimage.sh
+++ buildimage.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
-export IMAGENAME=nodejs-wikie
+export IMAGENAME=nodejs-wiki
 export BUILDROOTVER=2014.05
 
 
-mkdir ~/src
-cd ~/src
+CURDIR=`pwd`
+
+mkdir -p $CURDIR/docker-nodejs-wiki
+cd $CURDIR/docker-nodejs-wiki
+
+git clone https://github.com/lexlapax/dockerfile-tiny-nodejs-wiki
+
+mkdir src-buildroot
+cd src-buildroot
 
 wget http://buildroot.uclibc.org/downloads/buildroot-$BUILDROOTVER.tar.gz
 tar -xzvf buildroot-$BUILDROOTVER.tar.gz
-git clone https://github.com/lexlapax/dockerfile-tiny-nodejs-wiki
-cp -r dockerfile-tiny-nodejs-wiki/buildroot.config buildroot-$BUILDROOTVER/.config
+cp -r ../dockerfile-tiny-nodejs-wiki/buildroot.config buildroot-$BUILDROOTVER/.config
 cd buildroot-$BUILDROOTVER
 make all
 # wait a really long time while it builds everything including the toolchain
@@ -37,9 +43,12 @@ touch output/images/fixup/sbin/init output/images/fixup/etc/resolv.conf
 # add nodejs and wiki specific stuff here
 
 cd output/images
-cp root.tar fixup.tar
+cp rootfs.tar fixup.tar
 tar rvf fixup.tar -C fixup .
 
+cp fixup.tar $CURDIR/dockerfile-tiny-nodejs-wiki
+
+cd $CURDIR/dockerfile-tiny-nodejs-wiki
 # docker steps
-docker import - $IMAGENAME < fixup.tar
+docker build -t dockerfile-tiny-nodejs-wiki . 
 
