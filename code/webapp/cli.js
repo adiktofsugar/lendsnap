105eedbf546daed899c03ca3541848f54fbbe45b
S McCollum <smccollum@extole.com>
2014-11-11 01:21:07 -0800

    Trying to dockerize, but im not sure how containers communicate with each other...etc...

--- code/webapp/cli.js
+++ code/webapp/cli.js
@@ -4,46 +4,90 @@ var path = require("path");
 var cwd = __dirname;
 var spawn = require("child_process").spawn;
 
+var IMAGE_NAMES = ["nginx", "node", "database"];
+
 var withSpace = function (optionString) {
     if (optionString) {
         return " " + optionString;
     }
     return "";
 };
-var getCommand = {
-    "start": function (options) {
-        var optionString = "";
-        var command = "";
-        if (options.env == "dev") {
-            optionString = "-v " + cwd + ":/var/lendsnap " +
-                "-p 8080:80";
-        }
-        
-        return "docker run" + withSpace(optionString) +
-            " -d lwa";
+var getOptionString = function (isDev) {
+    var volume = isDev ? cwd + ":/var/lendsnap" : "/var/lendsnap";
+    return "-v " + volume;
+};
+var getCommands = {
+    "run": function (options) {
+        var optionString = getOptionString(options.env == "dev");
+        var imageNames = options.image ? [options.image] : IMAGE_NAMES;
+        var ports = {
+            "nginx": "2080:80",
+            "node" :"23000:3000",
+            "database": "23600:3606"
+        };
+        return imageNames.map(function (imageName) {
+            var port = ports[imageName];
+            var portOptionString = port ? "-p " + port : null;
+            return "docker run" + withSpace(optionString) +
+                withSpace(portOptionString) + " -d " + imageName;
+        });
     },
     "build": function (options) {
-        return "docker build -t lwa " + cwd;
+        return IMAGE_NAMES.map(function (imageName) {
+            return "docker build -t " + imageName + " " + cwd + "/" + imageName;
+        });
     },
     "console": function (options) {
-        var optionString = "-v " + cwd + ":/var/lendsnap " +
-                "-p 8080:80";
-        return "docker run -ti" + withSpace(optionString) +
-            " lwa /bin/bash";
+        var optionString = getOptionString(options.env == "dev");
+        var imageName = options.image;
+        return [
+            "docker run -ti" + withSpace(optionString) +
+                " " + imageName + " /bin/bash"
+        ];
     }
 };
 
 var runCommandFunction = function (commandName) {
-    var getCommandFunction = getCommand[commandName];
+    var getCommandFunction = getCommands[commandName];
 
     return function (options) {
-        var command = getCommandFunction(options);
-        console.log("Command: " + command);
-        
-        var commandArgs = command.split(" ");
-        spawn(commandArgs[0], commandArgs.slice(1), {
-            stdio: 'inherit'
-        });
+        var commands = getCommandFunction(options);
+        commands.reverse();
+
+        var runCommand = function () {
+            var command = commands.pop();
+            if (!command) {
+                return;
+            }
+            if (typeof command == "string") {
+                command = {
+                    command: command
+                };
+            }
+            var commandString = command.command;
+            console.log("Command: " + commandString);
+            
+            var completed = false;
+            var complete = function () {
+                if (completed) return;
+                completed = true;
+                runCommand();
+            };
+
+            var commandArgs = commandString.split(" ");
+            spawn(commandArgs[0], commandArgs.slice(1), {
+                stdio: 'inherit'
+            })
+            .on("error", function (error) {
+                if (!command.ignoreError) {
+                    throw error;
+                }
+            })
+            .on("close", function () {
+                complete();
+            });
+        };
+        runCommand();
     };
 };
 
@@ -52,13 +96,28 @@ nomnom.option('env', {
     default: "dev"
 });
 
+
 nomnom.command("build")
-    .help("Build the docker image")
+    .help("Build the docker images")
     .callback(runCommandFunction("build"));
-nomnom.command("start")
-    .help("Start the docker for the webapp")
-    .callback(runCommandFunction("start"));
+nomnom.command("run")
+    .options({
+        'image': {
+            abbr: 'i',
+            help: "Image to run - " + IMAGE_NAMES.join("|") + " (leave blank for all)",
+            choices: IMAGE_NAMES
+        }
+    })
+    .help("Run a command")
+    .callback(runCommandFunction("run"));
 nomnom.command("console")
+    .options({
+        'image': {
+            position: 1,
+            required: true,
+            help: "Image to console into - " + IMAGE_NAMES.join("|")
+        }
+    })
     .help("Open bash docker style (dev mode assumed)")
     .callback(runCommandFunction("console"));
 
